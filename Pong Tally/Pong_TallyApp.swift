60e73eaf0f35aad89fa5edd706a4a7f02aa2c146
Alek V <alekvasek@icloud.com>
2025-01-12 18:51:58 -0600

    Initial Commit

---
+++ Pong Tally/Pong_TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pong_TallyApp.swift
+//  Pong Tally
+//
+//  Created by Alek Vasek on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Pong_TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
